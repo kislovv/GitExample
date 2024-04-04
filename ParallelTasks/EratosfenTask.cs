86b11906f0cdb543ad4bf1944a0c846ad2496fa2
Kislov Kirill <kislov-k@inbox.ru>
2024-04-04 13:41:08 +0300

    add parallel tasks

---
+++ ParallelTasks/EratosfenTask.cs
@@ -0,0 +1,48 @@
+﻿namespace ParallelTasks;
+
+public class EratosfenTask(int n)
+{
+    public int N { get; set; } = n;
+    public int PrimeNumbersCount { get; set; } = 0;
+    public List<int> BasePrimeNumbers { get; set; } = [];
+
+    public void CountPrimeNumbers()
+    {
+        switch (N)
+        {
+            case 0:
+                return;
+            case < 3:
+                PrimeNumbersCount = N;
+                return;
+        }
+
+        PrimeNumbersCount = 2;
+        BasePrimeNumbers.AddRange([2,3]);
+        
+        var lastCheckedPrimaryNum = (int) Math.Ceiling(Math.Sqrt(N));
+        var primeNumbersCount = PrimeNumbersCount;
+        for (var i = 4; i < lastCheckedPrimaryNum; i++)
+        {
+            if (CheckPrimaryNum(i))
+            {
+                BasePrimeNumbers.Add(i);
+                primeNumbersCount++;
+            }
+        }
+        
+        Parallel.For(lastCheckedPrimaryNum, N + 1, i =>
+        {
+            if (CheckPrimaryNum(i))
+            {
+                Interlocked.Add(ref primeNumbersCount, 1);
+            }
+        });
+        PrimeNumbersCount = primeNumbersCount;
+    }
+
+    private bool CheckPrimaryNum(int num)
+    {
+        return BasePrimeNumbers.All(b => num % b != 0);
+    }
+}
---
+++ ParallelTasks/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ParallelTasks;
+
+var eratosfenTask = new EratosfenTask(100);
+
+eratosfenTask.CountPrimeNumbers();
+
+Console.WriteLine(eratosfenTask.PrimeNumbersCount);
