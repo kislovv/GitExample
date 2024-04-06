76df3f6d53f869d2337f0a4aae8b1fd5fff25f0c
Kislov Kirill <kislov-k@inbox.ru>
2024-04-06 12:19:04 +0300

    add avltree teamplate

---
+++ AVLTree/AVLNode.cs
@@ -0,0 +1,26 @@
+﻿namespace AVLTree;
+
+public class AVLNode<T>
+{
+    public AVLNode<T>? Parent { get; set; }
+    public AVLNode<T>? Left { get; set; }
+    public AVLNode<T>? Right { get; set; }
+    public required T Data { get; set; }
+
+    public int BalanceFactor => GetHeight(Right) - GetHeight(Left);
+
+    public int Height
+    {
+        get
+        {
+            var hl = GetHeight(Left);
+            var hr = GetHeight(Right);
+            return (hl > hr ? hl : hr ) + 1;
+        }
+    }
+
+    private static int GetHeight(AVLNode<T>? p)
+    {
+        return p?.Height ?? 0;
+    }
+}
---
+++ AVLTree/AvlTree.cs
@@ -0,0 +1,59 @@
+﻿namespace AVLTree;
+
+public class AvlTree<T>(T data)
+{
+    public AVLNode<T> Root { get; set; } = new()
+    {
+        Data = data
+    };
+    
+    public void Add(T Data)
+    {
+        var addedNode = new AVLNode<T>
+        {
+            Data = data
+        };
+        var current = Root;
+        while (true)
+        {
+            if (Comparer<T>.Default.Compare(
+                    current.Data, addedNode.Data) > 0)
+            {
+                if (current.Left != null)
+                {
+                    current = current.Left;
+                }
+                else
+                {
+                    current.Left = addedNode;
+                    //TODO: Balance
+                    break;
+                }
+            }
+            else
+            {
+                if (current.Right != null)
+                {
+                    current = current.Right;
+                }
+                else
+                {
+                    current.Right = addedNode;
+                    //TODO: Balance
+                    break;
+                }
+            }
+        }
+    }
+
+    private void Balance()
+    {
+        
+    }
+
+    private void RightRotation(AVLNode<T> subTree)
+    {
+        
+    }
+    
+}
