c6eadd5122c3a4660c2e25d270a137b48ad94c44
Kislov Kirill <kislov-k@inbox.ru>
2024-04-20 13:21:43 +0300

    add nuget package example

---
+++ ContractsExample/IModelService.cs
@@ -0,0 +1,14 @@
+﻿namespace ContractsExample;
+
+/// <summary>
+/// Интерфейс для взаимодействия с моделью ПП
+/// </summary>
+public interface IModelService
+{
+    /// <summary>
+    /// Метод обработки моделей 
+    /// </summary>
+    /// <param name="models">Входящие модели</param>
+    /// <returns>Строковое представление</returns>
+    string ProcessModels(Model[] models);
+}
---
+++ ContractsExample/Model.cs
@@ -0,0 +1,25 @@
+﻿namespace ContractsExample;
+
+/// <summary>
+/// Модель для задачи параллельного программирования
+/// </summary>
+public class Model
+{
+    /// <summary>
+    /// Данные 
+    /// </summary>
+    public string Data { get; set; }
+    /// <summary>
+    /// Номер модели
+    /// </summary>
+    public int Number { get; set; }
+    /// <summary>
+    /// Успешность обработки
+    /// </summary>
+    public bool IsSuccess { get; set; }
+
+    /// <summary>
+    /// Дата обработки
+    /// </summary>
+    public DateTime DateOfProcess { get; set; }
+}
