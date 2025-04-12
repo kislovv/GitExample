6b30b58919803e3af6b5c0ff38a45357bc40fec5
Kislov Kirill <kislov-k@inbox.ru>
2025-04-12 12:16:06 +0300

    add stack and streams

---
+++ LinkedList11407/MyStack.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace LinkedList11407;
+
+public class MyStack<T> : IMyStack<T>
+{
+    private Node<T>? _head;
+
+    public MyStack(T head)
+    {
+        _head = new Node<T>()
+        {
+            Data=head
+        };
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = _head;
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Push(T item)
+    {
+        _head ??= new Node<T>()
+        {
+            Data = item
+        };
+    }
+
+    public T Pop()
+    {
+        var head = _head;
+        if (_head!.Data != null)
+        { 
+            _head = null;
+        } 
+        return head!.Data;
+    }
+
+    public T Peek()
+    {
+        return _head!.Data;
+    }
+}
---
+++ Streams/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Streams;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FrequencyAnalysisOfTheText();
+    }
+
+    public static void FrequencyAnalysisOfTheText()
+    {
+        Dictionary<string, int> count = [];
+
+        using var sr = new StreamReader(@"input.txt");
+        string s;
+        while (!sr.EndOfStream)
+        {
+            s = sr.ReadLine()!;
+            foreach (var word in s.Split())
+            {
+                if (count.TryAdd(word, 1))
+                {
+                    continue;
+                }
+                count[word]++;
+            }
+        }
+        
+        using var sw = new StreamWriter(@"output.txt");
+        foreach (var word in count.Keys.OrderByDescending(key => count[key]))
+        {
+            sw.WriteLine($"{word} : {count[word]}");
+        }
+    }
+
+    public static void ReplaceOldToNew()
+    {
+        using var sr = new StreamReader(@"input.txt");
+        using var result = new StreamWriter(@"textWithNew.txt");
+        
+        string s;
+        while (!sr.EndOfStream)
+        {
+            s = sr.ReadLine()!;
+            s = s.Replace("старое", "новое");
+            result.WriteLine(s);
+        }
+    }
+}
